1639c1b1fb5e9be6e67eca5375470b6f8ac716e4
Patrick Cruz <145387737+cruzPatrick@users.noreply.github.com>
2026-02-11 15:24:32 -0300

    more stuff

--- Monitoria/src/aula4POO/Gato2.java
+++ Monitoria/src/aula4POO/Gato2.java
@@ -10,5 +10,7 @@ public class Gato2 extends Animal2{
 	public void emitirSom() {
 		System.out.println("Miauuuuu");
 	}
-	
+	public void ronronar() {
+		System.out.println("grrrrr");
+	}
 }
--- Monitoria/src/aula4POO/HerancaArrays.java
+++ Monitoria/src/aula4POO/HerancaArrays.java
@@ -7,6 +7,22 @@ public class HerancaArrays {
 		e.emitirSom();
 	}
 	
+	public static void facaAlgo (Animal2 e) {
+		if (e instanceof Cobra2) 
+			System.out.println("Eu sou uma cobra");
+		else 
+			System.out.println("Eu sou outro tipo de animal");
+	}
+	public static void facaAlgo1(Animal2 e) {
+		if(e instanceof Gato2) {
+			//e.ronronar(); não funcionar porque ele é um animal e não um gato completo, ronronar é exclusivo da classe filha e não da classe pai, se fosse emitir som até iria, pois tem override
+			Gato2 e2 = (Gato2)e;
+			e2.ronronar();
+		}
+		else
+			System.out.println("Nada...");
+	}
+	
 	public static void main(String[] args) {
 /*
 		Gato[] corrida1 = {new Gato(), new Gato(), new Gato()};
@@ -27,7 +43,7 @@ public class HerancaArrays {
 		
 		Animal2 m = new Gato2();
 		
-		System.out.println("----------(会いたくて - アド)--------------");
+		System.out.println("----------( 会いたくて - アド )--------------");
 		
 		Gato2 a = new Gato2();
 		Animal2 b = new Cobra2();
@@ -37,7 +53,22 @@ public class HerancaArrays {
 		testarSom(b);
 		testarSom(c);
 		
-
+		
+		System.out.println("----------( うっせえわ - アド )--------------");
+		//usando instaceof para ver como se porta com polimorfismo
+		facaAlgo(a);
+		facaAlgo(b);
+		
+		System.out.println("----------( 罪と罰 歌いました - アド )--------------");
+		
+		facaAlgo1(a);
+		facaAlgo1(b);
+		facaAlgo1(c);
+		
+		System.out.println("----------( Преступление и наказание - Фёдор Миха́йлович Достое́вский)--------------");
+		
+		
 	}
 
+	
 }
